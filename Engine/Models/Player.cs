c7f180ced6a3653f61a1939d1a2fc14eac9f5917
Dan Owen <DanOwen1@outlook.com>
2019-01-29 19:43:35 +0000

    Some updates to the game

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    /// <summary>
+    /// Character Class
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// Player's name
+        /// </summary>
+        public string PlayerName { get; set; }
+        /// <summary>
+        /// Player's class
+        /// </summary>
+        public int Class { get; set; }
+        /// <summary>
+        /// Player's level
+        /// </summary>
+        public int Level { get; set; }
+        /// <summary>
+        /// Player's hit points
+        /// </summary>
+        public int HitPoints { get; set; }
+        /// <summary>
+        /// Player's gold amount
+        /// </summary>
+        public int Gold { get; set; }
+        /// <summary>
+        /// Player's experience points
+        /// </summary>
+        public int ExperiencePoints { get; set; }
+
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        /// <summary>
+        /// Instatiation of the player 
+        /// </summary>
+        public Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.PlayerName = "Dan";
+            CurrentPlayer.Gold = 10000000;
+        }
+    }
+}
